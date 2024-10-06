90681513c61fe1ff0849bb16df0c5a373bf8715a
Katherina Dayaon <katherinadayaon@gmail.com>
2024-10-06 08:18:45 -0400

    aaaaa

--- knight_hacks_2024/lib/main.dart
+++ knight_hacks_2024/lib/main.dart
@@ -1,114 +1,144 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:usb_serial/transaction.dart';
+import 'package:usb_serial/usb_serial.dart';
 
-import 'register.dart';
-import 'reset.dart';
-import 'token.dart';
-import 'verification.dart';
-import 'homepage.dart';
-
-main() async{
-  WidgetsFlutterBinding.ensureInitialized;
-
-  await Firebase.initializeApp();
-
+void main() {
   runApp(
     const MaterialApp(
-      home: HomePageScreen(),
+      home: StartPage(),
     ),
   );
 }
 
-//Logo fades into Login
-class Start extends StatelessWidget {
-  const Start({super.key});
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(_createRoute());
-          },
-          child: const Text('Go!'),
-        ),
-      ),
-    );
-  }
+  createState() => _StartPageState();
 }
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+class _StartPageState extends State<StartPage> {
+ /* UsbPort? _port;
+  String _status = "Idle";
+  List<Widget> _ports = [];
+  List<Widget> _serialData = [];
 
-  @override
-  createState() => _LoginPageState();
-}
+  StreamSubscription<String>? _subscription;
+  Transaction<String>? _transaction;
+  UsbDevice? _device;
 
-String _email = '';
-String _password = '';
+  TextEditingController _textController = TextEditingController();
 
-class _LoginPageState extends State<LoginPage> {
-  void _handleEmailChange(String value) {
-    setState(() {
-      _email = value;
-    });
-  }
+  Future<bool> _connectTo(device) async {
+    _serialData.clear();
 
-  void _handlePasswordChange(String value) {
-    setState(() {
-      _password = value;
-    });
-  }
-
-  Future<void> _handleSubmit(context) async {
-    try {
-      if (_email == '' || _password == '') {
-        throw "Empty Email/Password Field";
-      }
-      await login(_email, _password);
-
-      if (verify == true) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => HomePageScreen()),
-        );
-      } else if (verify == false) {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const VerificationPage()),
-        );
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          backgroundColor: Colors.green, // Set background color to green
-          content: Center(
-            child: Text(
-              '$e', // Convert the error to a string to display
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontFamily: 'Montserrat',
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          duration: const Duration(seconds: 5), // Adjust the duration as needed
-        ),
-      );
+    if (_subscription != null) {
+      _subscription!.cancel();
+      _subscription = null;
     }
+
+    if (_transaction != null) {
+      _transaction!.dispose();
+      _transaction = null;
+    }
+
+    if (_port != null) {
+      _port!.close();
+      _port = null;
+    }
+
+    if (device == null) {
+      _device = null;
+      setState(() {
+        _status = "Disconnected";
+      });
+      return true;
+    }
+
+    _port = await device.create();
+    if (await (_port!.open()) != true) {
+      setState(() {
+        _status = "Failed to open port";
+      });
+      return false;
+    }
+    _device = device;
+
+    await _port!.setDTR(true);
+    await _port!.setRTS(true);
+    await _port!.setPortParameters(115200, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+    _transaction = Transaction.stringTerminated(_port!.inputStream as Stream<Uint8List>, Uint8List.fromList([13, 10]));
+
+    _subscription = _transaction!.stream.listen((String line) {
+      setState(() {
+        _serialData.add(Text(line));
+        if (_serialData.length > 20) {
+          _serialData.removeAt(0);
+        }
+      });
+    });
+
+    setState(() {
+      _status = "Connected";
+    });
+    return true;
+  }
+
+  void _getPorts() async {
+    _ports = [];
+    List<UsbDevice> devices = await UsbSerial.listDevices();
+    if (!devices.contains(_device)) {
+      _connectTo(null);
+    }
+    print(devices);
+
+    devices.forEach((device) {
+      _ports.add(ListTile(
+          leading: Icon(Icons.usb),
+          title: Text(device.productName!),
+          subtitle: Text(device.manufacturerName!),
+          trailing: ElevatedButton(
+            child: Text(_device == device ? "Disconnect" : "Connect"),
+            onPressed: () {
+              _connectTo(_device == device ? null : device).then((res) {
+                _getPorts();
+              });
+            },
+          )));
+    });
+
+    setState(() {
+      print(_ports);
+    });
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    UsbSerial.usbEventStream!.listen((UsbEvent event) {
+      _getPorts();
+    });
+
+    _getPorts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectTo(null);
+  }
+*/
+  @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-      body: Container(
+        body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [Color.fromRGBO(34, 34, 39, 1.0), Color.fromRGBO(93, 122, 173, 1.0)],
@@ -127,155 +157,144 @@ class _LoginPageState extends State<LoginPage> {
                   'Bloom Buddy',
                   style: TextStyle(
                   color: Color.fromRGBO(75, 95, 121, 1.0),
-                    fontWeight: FontWeight.w900,
+                    fontWeight: FontWeight.w100,
                     fontSize: 55,
                     fontFamily: 'Montserrat',
                   ),
                 ),
                 Image.asset(
                   'imgs/bloombuddylogotrans.png',
-                  width: 400,
+                  width: 300,
                   height: 400,
                 ),
-                const Text(
-                  'Welcome!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                    fontSize: 30,
-                    fontFamily: 'Montserrat',
-                  ),
-                ),
-                const SizedBox(height: 25.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 350, // Set width of the container
-                      child: Container(
-                        color: Colors.grey[200],
-                        child: TextFormField(
-                          onChanged: _handleEmailChange,
-                          obscureText: false,
-                          decoration: InputDecoration(
-                            hintText: 'Example@email.com',
-                            hintStyle: const TextStyle(
-                                color: Colors.black54, fontFamily: 'Montserrat'),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            contentPadding:
-                                const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 8.0),
-                          ),
-                        ),
-                      ),
-                    ),
+              ElevatedButton(
+          onPressed: () {
+            // Navigate to the main page when button is pressed
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MainPage()),
+            );
+          },
+          child: Text('Continue'),
+        ),
                   ],
                 ),
-                const SizedBox(height: 25.0),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      width: 350, // Set width of the container
-                      child: Container(
-                        color: Colors.grey[200],
-                        child: TextFormField(
-                          onChanged: _handlePasswordChange,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            hintText: 'Enter your password',
-                            hintStyle: const TextStyle(
-                                color: Colors.black54, fontFamily: 'Montserrat'),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            contentPadding:
-                                const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 8.0),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ResetPage()),
-                      );
-                    },
-                    child: const Text(
-                      'Forgot password?',
-                      style: TextStyle(color: Colors.blue, fontSize: 17),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                Builder(
-                  builder: (context) => MaterialButton(
-                    onPressed: () => _handleSubmit(context),
-                    color: const Color(0xFF14532d).withBlue(255),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 13.0),
-                    child: const SizedBox(
-                      width: 350,
-                      child: Text(
-                        'Login',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                            fontFamily: 'Montserrat'),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Register()),
-                    );
-                  },
-                  child: const Text(
-                    'Need an account? Sign up',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontFamily: 'Montserrat',
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ],
+                
             ),
           ),
         ),
       ),
-    ));
+    );
   }
 }
 
-Route _createRoute() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => const LoginPage(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      const begin = Offset(0.0, 1.0);
-      const end = Offset.zero;
-      const curve = Curves.ease;
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bloom Buddy'),
+        backgroundColor: Color.fromRGBO(75, 95, 121, 1.0), 
+      ),
+      body: Container(
+        // Set the gradient background using BoxDecoration
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromRGBO(34, 34, 39, 1.0), Color.fromRGBO(93, 122, 173, 1.0)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+          // Add a centered image
+          child: Center(
+            child: Image.network(
+            'imgs/bloombuddyplanttrans.png', // Example image URL
+            width: 500,
+            height: 500,
+          ),
+        ),
+      ),
+      // Text above the icons
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Plant Stats:',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
 
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // Temperature
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Temperature',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.thermostat,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      '50 degrees',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
 
-      return SlideTransition(
-        position: animation.drive(tween),
-        child: child,
-      );
-    },
-  );
+                // Brightness
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Brightness',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.sunny,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      '30%',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+
+                // Moisture Level
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Moisture Level',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.water_drop,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      '30%',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20), // Add some space at the bottom
+          ],
+        ),
+      ),
+    );
+  }
 }
